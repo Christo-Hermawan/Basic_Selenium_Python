3830ad879ad7cfc87abdad5f411048c978bee4c6
Christo-Hermawan <christo.hermawan08@gmail.com>
2024-08-08 21:59:56 +0700

    clicking link & navigating links (done)

--- main.py
+++ main.py
@@ -1,34 +1,48 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_condition as EC
-
+from selenium.webdriver.support import expected_conditions as EC
 import time
+import random
 
+# path to Edge WebDriver
 service = Service(executable_path="chromedriver.exe")
-# drive = webdriver.Chrome(service=service)
 
-driver = webdriver.Chrome()
+# initialize WebDriver
+driver = webdriver.Chrome(service=service)
 
-driver.get("https://google.com")
+driver.get("https://www.bing.com")
 
-# preferably
+# step 1 - Wait for the element to show up (random delay added)
+delay = random.uniform(2.0, 5.0)  # Delay between 2 to 5 seconds
+time.sleep(delay)
 
-# step 1 - wait until the elements shows up
+# step 2 - wait until the elements shows up
+# wait if after 10s until the element with the class name shows up
+# if the element doesn't show, shut the program
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "sb_form_q"))
+)
 
-WebDriverWait(driver, 5).until( 
-    EC.presence_of_element_located((By.CLASS_NAME, "gLFyF"))
-)  # to wait if after 5s until the element with the class name shows up
-
-# step 2 - then do the thing
-
-input_element = driver.find_element(By.CLASS_NAME, "gLFyf") # find the element by class name
+# step 3 - then do the activity
+input_element = driver.find_element(By.ID, "sb_form_q") # find the element by class name
 input_element.clear() # clear the elements incase the field not empty / have default value
-input_element.send_keys("test selenium" + Keys.ENTER) # send text/input + enter 
+time.sleep(delay)
+input_element.send_keys("youtube" + Keys.ENTER) # send text/input + enter 
 
 
-time.sleep(10)
+# if the link text to exist then continue
+WebDriverWait(driver, 20).until(
+    EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "YouTube"))
+)
+
+time.sleep(delay)
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "YouTube")
+link.click();
+
+time.sleep(50)
 
 driver.quit()
 
