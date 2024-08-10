d0fe5dd264c88f606dd9cd98158ef1a5dd2dc1c2
Christo-Hermawan <christo.hermawan08@gmail.com>
2024-08-10 23:31:10 +0700

    add cookieclicker.py

---
+++ cookieclickers.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import random
+
+Service = Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+cookie_id = "bigCookie"
+
+cookie = driver.find_element(By.ID, cookie_id)
+cookie.click()
