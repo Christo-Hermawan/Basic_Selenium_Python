76f24e97cab385884406362a6e36a5ce3c75d70c
Christo-Hermawan <christo.hermawan08@gmail.com>
2024-08-12 14:15:24 +0700

    automating cookie clickers (done)

--- cookieclickers.py
+++ cookieclickers.py
@@ -7,12 +7,46 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import random
 
-Service = Service(executable_path="chromedriver.exe")
+service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
 cookie_id = "bigCookie"
+cookies_id =  "cookies"
+product_price_prefix = "productPrice"
+product_prefix = "product"
 
-cookie = driver.find_element(By.ID, cookie_id)
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'English')]"))
+)
+
+language = driver.find_element(By.XPATH, "//*[contains(text(), 'English')]")
+language.click()
+
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, cookie_id))
+)
+
+cookie = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, cookie_id)))
 cookie.click()
+
+while True:
+    cookie.click()
+    cookies_count = driver.find_element(By.ID, cookies_id).text.split(" ")[0]
+    cookies_count = int(cookies_count.replace(",", ""))
+
+    for i in range(4):
+        product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",","")
+
+        if not product_price.isdigit():
+            continue
+        product_price = product = int(product_price) 
+
+        if cookies_count >= product_price:
+            product = driver.find_element(By.ID, product_prefix + str(i))
+            product.click()
+            break
+    
+
+
